43597581f412cf143eab7cd9bf62a6d41cd93828
Gregory Pfeil <greg@technomadic.org>
2018-11-29 13:07:45 -0700

    Add a Haskell interpreter for `Nu`.

---
+++ haskell/src/Dada.hs
@@ -0,0 +1,24 @@
+module Dada where
+
+import           Control.Arrow
+import qualified Data.HashMap.Strict.InsOrd as H
+import qualified Dhall as D
+import qualified Dhall.Core as D
+import qualified Yaya.Control as Y
+
+-- | This is the key to a Dhall runtime – `Nu` is the one thing that can’t be
+--   normalized away.
+corecursive :: Y.Corecursive t f => D.Type (a -> f a) -> D.Type a -> D.Type t
+corecursive ψ a =
+  D.Type
+  (\case
+      D.RecordLit fields ->
+        Y.ana
+        <$> D.extract ψ =<< H.lookup "coalgebra" fields
+        <*> D.extract a =<< H.lookup "seed" fields
+      _ -> Nothing)
+  (D.Record (H.fromList [("coalgebra", D.expected ψ), ("seed", D.expected a)]))
+
+instance (Y.Corecursive t f, D.Interpret (a -> f a), D.Interpret a) =>
+         D.Interpret t where
+  autoWith = uncurry corecursive . (autoWith &&& autoWith)
